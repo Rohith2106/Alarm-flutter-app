59e50be8d6d6884afa3df73367adb03d99237306
rohith <rohithkumar21062002@gmail.com>
2024-06-15 22:00:55 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'dart:async';
+import 'package:cod_soft_alarm/Home.dart';
+import 'package:flutter/material.dart';
+import 'package:alarm/alarm.dart';
+import 'package:flutter/services.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
+  await Alarm.init(showDebugLogs: true);
+
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          scaffoldBackgroundColor: Color.fromARGB(255, 137, 96, 193),
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: Colors.grey, brightness: Brightness.dark)),
+      home: const ExampleAlarmHomeScreen(),
+    ),
+  );
+}
